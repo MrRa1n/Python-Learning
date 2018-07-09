c917e8050b86b95ec1920a18d7f4a21ee72fd010
MrRa1n <tobycookni@gmail.com>
2018-07-09 19:37:05 +0100

    My first Python commit with some Hello World examples

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+print("Hello, world!")
+print("This is a Python program.")
+
+# Store text in variable
+s = "Hello, world!"
+print(s)
+
+# Read input from user
+#name = input("Enter name: ")
+#print(name)
+
+sentence = "The cat is brown"
+q = "cat"
+
+if q != sentence:
+    print("strings equal")
